f0dc064d274bd22838c04cbd9253f049e791c49b
Ontheway361 <jielu361@163.com>
2019-04-04 10:09:49 +0800

    version-1.0 |debug+

--- model_trainer.py
+++ model_trainer.py
@@ -5,12 +5,13 @@ Created on 2019/04/02
 author: lujie
 """
 
+import torch
 from utils.p3d_zoo import *
 
 
 if __name__ == '__main__':
 
-    model = P3D199(pretrained=True,num_classes=400)
+    model = P3D199(pretrained=False, num_classes=101)
     model = model.cuda()
     data = torch.autograd.Variable(torch.rand(10,3,16,160,160)).cuda()   # if modality=='Flow', please change the 2nd dimension 3==>2
     out = model(data)
